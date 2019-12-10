1a45b7ea4cc83d42502bc7fb4fe24893b0251fef
zhu <521052232@qq.com>
2019-12-10 15:44:40 +0800

    队列

---
+++ Neuedu_20191210_队列/ArrayQueue.java
@@ -0,0 +1,77 @@
+package Neuedu_20191210_队列;
+
+import java.util.Arrays;
+
+public class ArrayQueue {
+
+    // 使用数组实现的顺序队列
+    private String[] items;
+
+    // 数组大小
+    private int n = 0;
+
+    // head表示队头下标
+    private int head = 0;
+
+    // tail表示队尾下标
+    private int tail = 0;
+
+    public ArrayQueue(int capacity) {
+        items = new String[capacity];
+        n = capacity;
+    }
+
+    /**
+     * 入队操作
+     * @param item 数组实现的队列
+     * @return true:入队成功 false:入队失败
+     * 缺点：tail指针会持续往后移动，当tail移动到最右边，即使数组还有空闲空间，也无法向队列里面添加数据
+
+        public boolean enqueue(String item){
+            if(tail == n)return false;
+            items[tail] = item;
+            ++tail;
+            return true;
+        }
+
+     */
+
+    public boolean enqueue(String item){
+        if(tail == n){
+            // 如果满足tail == n && head == 0,表示队列已满。若满足tail == n 但head != 0 表示队列未满，需要进行数据搬移操作
+            if(head == 0)return false;
+            // 接下来进行数据搬移操作
+            for(int i = head; i<tail; i++){
+                items[i-head] = items[i];
+            }
+            // 搬移操作后对头尾指针进行充值
+            tail -= head;
+            head = 0;
+        }
+        items[tail] = item;
+        ++tail;
+        return true;
+    }
+
+    /**
+     * 出队操作
+     * @return  出队的元素
+     */
+    public String dequeue(){
+        // 如果队头下标等于队尾下标，表示队列为空
+        if(head == tail)return null;
+        String ret = items[head];
+        ++head;
+        return ret;
+    }
+
+    @Override
+    public String toString() {
+        return "ArrayQueue{" +
+                "items=" + Arrays.toString(items) +
+                ", n=" + n +
+                ", head=" + head +
+                ", tail=" + tail +
+                '}';
+    }
+}
---
+++ Neuedu_20191210_队列/test.java
@@ -0,0 +1,24 @@
+package Neuedu_20191210_队列;
+
+public class test {
+    public static void main(String[] args) {
+        ArrayQueue queue = new ArrayQueue(5);
+
+        // 五个数据入队,此时head=0,tail=5
+        queue.enqueue("猪");
+        queue.enqueue("哈哈");
+        queue.enqueue("哈");
+        queue.enqueue("嘻嘻");
+        queue.enqueue("嘻");
+
+        // 两个数据出队,此时head=2,tail=5
+        queue.dequeue();
+        queue.dequeue();
+
+        // 一个数据入队,检测到tail=5,且head!=0,所以进行数据搬移操作,搬移后head=0,tail=4
+        queue.enqueue("ddd");
+
+        System.out.println(queue.toString());
+
+    }
+}
